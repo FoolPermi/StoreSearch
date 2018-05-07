be2d2b28b021e47847b33594840d6688559cd6cc
foolpermi <foolpermi@gmail.com>
2018-05-07 22:42:06 +0800

    Add Fake data for search result.

---
+++ StoreSearch/StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Permi on 2018/5/7.
+//  Copyright © 2018 Permi. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
