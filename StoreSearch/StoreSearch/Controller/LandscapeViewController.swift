467f5d23cb2021cb8a27971ec33f603ff73393c9
FoolPermi <foolpermi@gmail.com>
2018-05-22 18:56:04 +0800

    tmp commit, unfinished

--- StoreSearch/StoreSearch/Controller/LandscapeViewController.swift
+++ StoreSearch/StoreSearch/Controller/LandscapeViewController.swift
@@ -10,26 +10,91 @@ import UIKit
 
 class LandscapeViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    var searchResults = [SearchResult]()
+    private var firstTime = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        view.removeConstraints(view.constraints)
+        view.translatesAutoresizingMaskIntoConstraints = true
+        
+        scrollView.removeConstraints(scrollView.constraints)
+        scrollView.translatesAutoresizingMaskIntoConstraints = true
+        
+        pageControl.removeConstraints(pageControl.constraints)
+        pageControl.translatesAutoresizingMaskIntoConstraints = true
+        
+        scrollView.backgroundColor = UIColor(patternImage: UIImage(named: "LandscapeBackground")!)
+        
+        
+        
     }
-
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        scrollView.frame = view.bounds
+        pageControl.frame = CGRect(x: 0, y: view.frame.size.height - pageControl.frame.size.height, width: view.frame.size.width, height: pageControl.frame.size.height)
+        
+        if firstTime {
+            firstTime = false
+            titleButtons(searchResults)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private func titleButtons(_ searchResults: [SearchResult]) {
+        var columnPerPage = 5
+        var rowPerPage = 3
+        var itemWidth: CGFloat = 96
+        var itemHeight: CGFloat = 88
+        var marginX: CGFloat = 0
+        var marginY: CGFloat = 20
+        
+        let viewWidth = scrollView.bounds.size.width
+        
+        switch viewWidth {
+        case 568:
+            columnPerPage = 6
+            itemWidth = 94
+            marginX = 2
+        case 667:
+            columnPerPage = 7
+            itemWidth = 95
+            itemHeight = 98
+            marginX = 1
+            marginY = 29
+        case 736:
+            columnPerPage = 8
+            rowPerPage = 4
+            itemWidth = 92
+        default:
+            break
+        }
+        // Button size
+        let buttonWidth: CGFloat = 82
+        let buttonHeight: CGFloat = 82
+        let paddingHorz = (itemWidth - buttonWidth) / 2
+        let paddingVert = (itemHeight - buttonHeight) / 2
+        // Add Buttons
+        var row = 0
+        var column = 0
+        var x = marginX
+        for (index, result) in searchResults.enumerated() {
+            let button = UIButton(type: .system)
+            button.backgroundColor = UIColor.white
+            button.setTitle("\(index)", for: .normal)
+            
+        }
+        
     }
-    */
 
 }
+
